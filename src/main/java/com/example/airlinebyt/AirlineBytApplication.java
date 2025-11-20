261272a2457c1ba3475012c66344751d7e660cf0
s30069 <s30069@pjwstk.edu.pl>
2025-11-20 19:19:05 +0100

    initial commit

---
+++ src/main/java/com/example/airlinebyt/AirlineBytApplication.java
@@ -0,0 +1,13 @@
+package com.example.airlinebyt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineBytApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirlineBytApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/airlinebyt/AirlineBytApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airlinebyt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineBytApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
