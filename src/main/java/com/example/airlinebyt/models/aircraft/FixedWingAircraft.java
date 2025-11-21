7c6fcfe99953a38e9b801003b430a4ee5e655d69
arsenizar <arseniizarudniuk@gmail.com>
2025-11-21 22:08:08 +0100

    removed everything except models and enums; made models empty

--- src/main/java/com/example/airlinebyt/models/aircraft/FixedWingAircraft.java
+++ src/main/java/com/example/airlinebyt/models/aircraft/FixedWingAircraft.java
@@ -1,16 +1,4 @@
 package com.example.airlinebyt.models.aircraft;
 
-import jakarta.persistence.Entity;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-
-@Entity
-@Data
-@EqualsAndHashCode(callSuper = true)
-@NoArgsConstructor
 public class FixedWingAircraft extends Aircraft {
-
-    private Double wingLength;
-    private int landingWheels;
 }
